93df616f038e2c492cb716071ab87f66e8cbc738
MaanuelMM <manuel.martin.malagon@alumnos.upm.es>
2019-05-10 23:44:50 +0200

    Remove alpine versions and 9.2 version

--- update.sh
+++ update.sh
@@ -10,22 +10,13 @@ if [ ${#versions[@]} -eq 0 ]; then
 fi
 versions=( "${versions[@]%/Dockerfile}" )
 
-packagesUrlJessie='http://apt.postgresql.org/pub/repos/apt/dists/jessie-pgdg/main/binary-amd64/Packages'
-packagesJessie="$(echo "$packagesUrlJessie" | sed -r 's/[^a-zA-Z.-]+/-/g')"
-curl -sSL "${packagesUrlJessie}.bz2" | bunzip2 > "$packagesJessie"
-
-packagesUrlStretch='http://apt.postgresql.org/pub/repos/apt/dists/stretch-pgdg/main/binary-amd64/Packages'
-packagesStretch="$(echo "$packagesUrlStretch" | sed -r 's/[^a-zA-Z.-]+/-/g')"
-curl -sSL "${packagesUrlStretch}.bz2" | bunzip2 > "$packagesStretch"
+packagesUrl='http://apt.postgresql.org/pub/repos/apt/dists/stretch-pgdg/main/binary-amd64/Packages'
+packages="$(echo "$packagesUrl" | sed -r 's/[^a-zA-Z.-]+/-/g')"
+curl -sSL "${packagesUrl}.bz2" | bunzip2 > "$packages"
 
 travisEnv=
 for version in "${versions[@]}"; do
 	IFS=- read pg_major postgis_major <<< "$version"
-	if [[ $pg_major = 9* ]]; then
-		packages="$packagesStretch"
-	else
-		packages="$packagesStretch"
-	fi
 
 	fullVersion="$(grep -m1 -A10 "^Package: postgresql-$pg_major-postgis-$postgis_major\$" "$packages" | grep -m1 '^Version: ' | cut -d' ' -f2)"
 	[ -z "$fullVersion" ] && { echo >&2 "Unable to find package for PostGIS $postgis_major on Postgres $pg_major"; exit 1; }
@@ -36,25 +27,9 @@ for version in "${versions[@]}"; do
 		sed -i 's/%%PG_MAJOR%%/'$pg_major'/g; s/%%POSTGIS_MAJOR%%/'$postgis_major'/g; s/%%POSTGIS_VERSION%%/'$fullVersion'/g' "$version/Dockerfile"
 	)
 
-	srcVersion="${fullVersion%%+*}"
-	srcSha256="$(curl -sSL "https://github.com/postgis/postgis/archive/$srcVersion.tar.gz" | sha256sum | awk '{ print $1 }')"
-	for variant in alpine; do
-		if [ ! -d "$version/$variant" ]; then
-			continue
-		fi
-		(
-			set -x
-			cp Dockerfile.alpine.template initdb-postgis.sh update-postgis.sh "$version/$variant/"
-			mv "$version/$variant/Dockerfile.alpine.template" "$version/$variant/Dockerfile"
-			sed -i 's/%%PG_MAJOR%%/'"$pg_major"'/g; s/%%POSTGIS_VERSION%%/'"$srcVersion"'/g; s/%%POSTGIS_SHA256%%/'"$srcSha256"'/g' "$version/$variant/Dockerfile"
-		)
-		travisEnv="\n  - VERSION=$version VARIANT=$variant$travisEnv"
-	done
-
 	travisEnv='\n  - VERSION='"$version$travisEnv"
 done
 travis="$(awk -v 'RS=\n\n' '$1 == "env:" { $0 = "env:'"$travisEnv"'" } { printf "%s%s", $0, RS }' .travis.yml)"
 echo "$travis" > .travis.yml
 
-rm "$packagesJessie"
-rm "$packagesStretch"
+rm "$packages"
